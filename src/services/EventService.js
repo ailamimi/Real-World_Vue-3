08c56eb0ec79971e3c158f50e51bc7dc7b3b449b
ailamimi <nurailasyamimi201@gmail.com>
2022-11-04 10:40:26 +0800

    new push

---
+++ src/services/EventService.js
@@ -0,0 +1,21 @@
+import axios from 'axios'
+
+/*
+ * apiClient - single axios instance for our app
+ * baseUrl - base URL for all calls to use
+ * Content-Type - for authentication & configuration
+ */
+const apiClient = axios.create({
+  baseURL: 'https://my-json-server.typicode.com/ailamimi/Real-World_Vue-3',
+  withCredentials: false,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
+  }
+})
+
+export default {
+  getEvents() {
+    return apiClient.get('/events')
+  }
+}
