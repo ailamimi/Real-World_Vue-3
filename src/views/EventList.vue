79c5f7ad32666ad8281522ab3e551a030b871245
ailamimi <nurailasyamimi201@gmail.com>
2022-11-04 17:13:00 +0800

    new

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -3,6 +3,21 @@
     <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
     <h1>Events for good</h1>
     <EventCard v-for="event in events" :key="event.id" :event="event" />
+
+    <router-link
+      :to="{ name: 'EventList', query: { page: page - 1 } }"
+      rel="prev"
+      v-if="page != 1"
+    >
+      Prev Page
+    </router-link>
+
+    <router-link
+      :to="{ name: 'EventList', query: { page: page + 1 } }"
+      rel="next"
+    >
+      Next Page
+    </router-link>
   </div>
 </template>
 
